592ee9b3a6b41b032f77c93569ed3fcb8f388a74
iliag <iliag@hedge-tech.com>
2025-10-10 11:16:23 +0300

    created connection with cloudinary, created message and user controller, router

---
+++ server/controllers/messageController.js
@@ -0,0 +1,94 @@
+// Get all users except the logged user
+
+import User from "../models/User.js";
+import Message from "../models/Message.js";
+import cloudinary from "../lib/cloudinary.js";
+
+export const getUsersForSidebar = async (req, res) => {
+    try {
+        const currentUserId = req.user._id;
+
+        const filteredUsers = User.find({
+            _id: {$ne: currentUserId}
+        }).select("-password");
+
+        // Count number of messages not seen
+        const unseenMessages = {};
+        const promises = filteredUsers.map(async (user) => {
+            const messages = await Message.find({
+                senderId: user._id,
+                receiverId: currentUserId,
+                seen: false
+            });
+
+            if (messages.length > 0) {
+                unseenMessages[user._id] = messages.length;
+            }
+        });
+
+        await Promise.all(promises);
+        res.json({success: true, filteredUsers, unseenMessages});
+    } catch (error) {
+        console.error(error.message);
+        res.json({success: false, message: error.message});
+    }
+}
+
+// Get all messages for selected user
+export const getMessages = async (req, res) => {
+    try {
+        const {id: selectedUserId} = req.params;
+        const currentUserId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                {senderId: currentUserId, receiverId: selectedUserId},
+                {senderId: selectedUserId, receiverId: currentUserId}
+            ]
+        }).sort({createdAt: 1});
+
+        await Message.updateMany({senderId: selectedUserId, receiverId: currentUserId}, {seen: true})
+
+        res.json({success: true, messages});
+    } catch (error) {
+        console.error(error.message);
+        res.json({success: false, message: error.message});
+    }
+}
+
+// api to mark messages as seen
+export const markMessagesAsSeen = async (req, res) => {
+    try {
+        const {id} = req.params;
+        await Message.findByIdAndUpdate(id, {seen: true});
+
+        await Message.updateMany({senderId: selectedUserId, receiverId: currentUserId}, {seen: true})
+        res.json({success: true, message: "Messages marked as seen"});
+    } catch (error) {
+        console.error(error.message);
+        res.json({success: false, message: error.message});
+    }
+}
+
+// Send message to selected user
+export const sendMessage = async (req, res) => {
+    try {
+        const {text, image} = req.body;
+        const receiverId = req.params.id;
+        const senserId = req.user._id;
+
+        let imageUrl;
+        if (image) {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+        const newMessage = await Message.create({
+            senderId, receiverId, text, image: imageUrl
+        })
+
+        res.json({success: true, newMessage});
+    } catch (error) {
+        console.error(error.message);
+        res.json({success: false, message: error.message});
+    }
+}
---
+++ server/controllers/userController.js
@@ -0,0 +1,92 @@
+import User from "../models/User.js";
+
+import bcrypt from "bcryptjs";
+import {generateToken} from "../lib/utils.js";
+import cloudinary from "../lib/cloudinary.js";
+
+// Signup a new user
+export const signup = async (req, res) => {
+    const {fullName, email, password, bio} = req.body;
+
+    try {
+        if (!fullName || !email || !password) {
+            return res.json({success: false, error: "Missing Details"});
+        }
+        const user = await User.findOne({email});
+
+        if (user) {
+            return res.json({success: false, error: "Account already exists"});
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullName, email, password: hashedPassword, bio
+        })
+
+        const token = generateToken(newUser._id)
+
+        const savedUser = await newUser.save();
+        const {password, ...userData} = savedUser.toObject();
+
+        res.json({success: true, userData, token, message: "Account created successfully"})
+    } catch (error) {
+        console.error(error.message);
+        res.json({success: false, message: error.message});
+    }
+}
+
+// Controller for user login
+export const login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const userData = await User.findOne({email});
+
+        if (!userData) return res.json({success: false, error: "User not found"});
+
+        const isPasswordCorrect = await bcrypt.compare(password, userData.password);
+
+        if (!isPasswordCorrect) return res.json({success: false, error: "Invalid Credentials"});
+
+        const token = generateToken(userData._id);
+        const {password: pwd, ...user} = userData.toObject();
+
+        res.json({success: true, user, token, message: "Login Successful"})
+    } catch (error) {
+        console.error(error.message);
+        res.json({success: false, message: error.message});
+    }
+}
+
+// Controller to check if user is authenticated
+export const checkAuth = async (req, res) => {
+    res.json({success: true, user: req.user})
+}
+
+// Controller to update user profile details
+export const updateProfile = async (req, res) => {
+    try {
+        const {fullName, bio, profilePic} = req.body;
+
+        const userId = req.user._id;
+        let updatedUser;
+
+        if (!profilePic) {
+            User.findByIdAndUpdate(userId, {fullName, bio}, {new: true});
+        } else {
+            const upload = await cloudinary.uploader.upload(profilePic);
+
+            updatedUser = await User.findByIdAndUpdate(userId, {
+                profilePic: upload.secure_url,
+                fullName,
+                bio
+            }, {new: true});
+        }
+
+        res.json({success: true, user: updatedUser});
+    } catch (error) {
+        console.error(error.message);
+        res.json({success: false, message: error.message});
+    }
+}
